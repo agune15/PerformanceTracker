bf5205005f0d9459001b06d13ba4f90d0a4fad10
agune15 <alex.nebot.sds@gmail.com>
2021-11-28 14:00:51 +0100

    (WIP) CPU tracking logic

--- PerformanceTracker.py
+++ PerformanceTracker.py
@@ -5,9 +5,8 @@ from classes import CPUTracker
 def main():
     cpu_tracker = CPUTracker.CPUTracker()
     while(True):
-        print("CPU usage: {perc}%".format(perc=cpu_tracker.get_usage_percentage()))
         time.sleep(1)
+        cpu_tracker.track_performance(70)
 
 if __name__ == "__main__":
     main()
-
--- classes/CPUTracker.py
+++ classes/CPUTracker.py
@@ -10,17 +10,32 @@ class CPUTracker:
     def __init__(self):
         self.last_working_time, self.last_idle_time = self.get_cpu_times()
 
-    def get_cpu_times(self):
-        with open(CPU_STATS_PATH) as statfile:
-            cpustats = statfile.readline().split()
-            working_time = int(cpustats[1]) + int(cpustats[2]) + int(cpustats[3])
-            idle_time = int(cpustats[4] + cpustats[5])
-            return working_time, idle_time
+    def track_performance(self, threshold):
+        if not (0 <= threshold <= 100):
+            raise AssertionError("CPU usage percentage has to be between 0 and 100")
+
+        # get list of processes
+        cpu_usage = self.get_usage_percentage()
+        if (cpu_usage > threshold):
+            print(f"CPU usage exceeds threshold: {round(cpu_usage,2)}% > {threshold}")
+            # Fill al log
 
     def get_usage_percentage(self):
         curr_working_time, curr_idle_time = self.get_cpu_times()
         working_time = curr_working_time - self.last_working_time
         idle_time = curr_idle_time - self.last_idle_time
         total_time = working_time + idle_time
+
+        self.last_working_time = curr_working_time
+        self.last_idle_time = curr_idle_time
+
         usage_perc = (working_time / total_time) * 100
         return usage_perc
+
+    def get_cpu_times(self):
+        with open(CPU_STATS_PATH) as statfile:
+            cpustats = statfile.readline().split()
+            working_time = int(cpustats[1]) + int(cpustats[2]) + int(cpustats[3])
+            idle_time = int(cpustats[4]) + int(cpustats[5])
+            return working_time, idle_time
+
